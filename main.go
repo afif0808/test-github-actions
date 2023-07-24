2cf08ab611b9d55e3ab79347ea5dcb90cfc6eea8
Just Another Fif <31957520+afif0808@users.noreply.github.com>
2023-07-24 08:36:14 +0700

    Update main.go

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
-	fmt.Println("Hello")
-	fmt.Println("Hello")
+	fmt.Println("Hello Hello")
+	fmt.Println("Hello Hi")
+	fmt.Println("Hi Hello")
 	fmt.Println("Hello")
 	fmt.Println("Hello")
 	fmt.Println("Hello")
